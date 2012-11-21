9d23871f2a0627eaae3ebaf591632f8ffc4905d0
Pieter Kokx <pieter@kokx.nl>
2012-11-21 12:08:06 +0100

    Moved ReadWriteRun into a separate class.

--- ChatServer.java
+++ ChatServer.java
@@ -2,32 +2,6 @@ import java.io.*;
 import java.util.*;
 import java.net.*;
 
-class ReadWriteRun implements Runnable {
-    PrintStream out;
-    BufferedReader in;
-    String prefix;
-
-    public ReadWriteRun(BufferedReader in, PrintStream out, String prefix)
-    {
-        this.in = in;
-        this.out = out;
-        this.prefix = prefix;
-    }
-
-    public void run()
-    {
-        String input = "";
-
-        try {
-            while ((input = in.readLine()) != null) {
-                out.println(prefix + input);
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-}
-
 class ChatServer {
 
     public final static int PORT = 25665;
