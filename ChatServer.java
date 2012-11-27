21d49d6f2b0bc2d3a3175cc1e52dae3e0890afb0
Pieter Kokx <pieter@kokx.nl>
2012-11-27 10:50:33 +0100

    Made all the ChatServer classes inner classes.

--- ChatServer.java
+++ ChatServer.java
@@ -4,16 +4,8 @@ import java.net.*;
 
 import com.google.protobuf.ByteString;
 
-// represents a connection to a client
-class Client
-{
-    Socket sock;
 
-    OutputStream out;
-    InputStream in;
-
-    // reader
-    ReadRun read;
+class ChatServer {
 
     static byte[] intToByteArray(int value)
     {
@@ -25,124 +17,133 @@ class Client
         };
     }
 
-    public Client(ServerSocket server) throws Exception
+    // represents a connection to a client
+    class Client
     {
-        if ((sock = server.accept()) == null) {
-            throw new Exception("Connection failed.");
+        Socket sock;
+
+        OutputStream out;
+        InputStream in;
+
+        // reader
+        ReadRun read;
+
+        public Client(ServerSocket server) throws Exception
+        {
+            if ((sock = server.accept()) == null) {
+                throw new Exception("Connection failed.");
+            }
+
+            out = sock.getOutputStream();
+            in = sock.getInputStream();
         }
 
-        out = sock.getOutputStream();
-        in = sock.getInputStream();
-    }
-
-    /**
-     * Get the IP address from this client.
-     */
-    byte[] getIp()
-    {
-        return sock.getInetAddress().getAddress();
-    }
-
-    // expect a reply
-    void expectReply()
-    {
-        read = new ReadRun(in);
-        new Thread(read).start();
-    }
-
-    boolean hasReply()
-    {
-        return read.wasRead;
-    }
-
-    void write(com.google.protobuf.GeneratedMessage message)
-    {
-        try {
-            int len = message.toByteArray().length;
-            byte[] length = intToByteArray(len);
-
-            out.write(length);
-            message.writeTo(out);
-        } catch (IOException e) {
-            System.out.println("I/O Error");
-            System.exit(-1);
+        /**
+         * Get the IP address from this client.
+         */
+        byte[] getIp()
+        {
+            return sock.getInetAddress().getAddress();
         }
-    }
-}
 
-// represents the server
-class Server {
+        // expect a reply
+        void expectReply()
+        {
+            read = new ReadRun(in);
+            new Thread(read).start();
+        }
 
-    ServerSocket sock = null;
+        boolean hasReply()
+        {
+            return read.wasRead;
+        }
 
-    ArrayList<Client> clients = new ArrayList<Client>();
+        void write(com.google.protobuf.GeneratedMessage message)
+        {
+            try {
+                int len = message.toByteArray().length;
+                byte[] length = intToByteArray(len);
 
-    /**
-     * Constructor.
-     */
-    public Server(int port) throws IOException
-    {
-        sock = new ServerSocket(port);
-    }
-
-    /**
-     * Wait for a client to connect.
-     */
-    public void connectClient() throws Exception
-    {
-        clients.add(new Client(sock));
-    }
-
-    public void waitClientReply() throws InterruptedException
-    {
-        // wait for all clients to have a reply
-        boolean readAll = false;
-        while (!readAll) {
-            Thread.sleep(ChatServer.TIME_POLL);
-
-            readAll = true;
-            for (Client c : clients) {
-                readAll = readAll && c.hasReply();
+                out.write(length);
+                message.writeTo(out);
+            } catch (IOException e) {
+                System.out.println("I/O Error");
+                System.exit(-1);
             }
         }
     }
 
-    /**
-     * Initialize the token ring.
-     */
-    public void init(int initialPort) throws InterruptedException
-    {
-        // give everyone the proper port number
-        for (int i = 0; i < clients.size(); i++) {
-            // create the message
-            ChatProto.Init init = ChatProto.Init.newBuilder()
-                                                .setPort(initialPort + i)
-                                                .build();
-            clients.get(i).write(init);
+    // represents the server
+    class Server {
 
-            clients.get(i).expectReply();
+        ServerSocket sock = null;
+
+        ArrayList<Client> clients = new ArrayList<Client>();
+
+        /**
+         * Constructor.
+         */
+        public Server(int port) throws IOException
+        {
+            sock = new ServerSocket(port);
         }
 
-        waitClientReply();
-
-        // link all clients
-        for (int i = 0; i < clients.size(); i++) {
-            ByteString ip = ByteString.copyFrom(clients.get(i).getIp());
-            ChatProto.ConnectTo message = ChatProto.ConnectTo.newBuilder()
-                .setIp(ip)
-                .setPort((i + 1) % clients.size() + initialPort)
-                .setInit(i == 0)
-                .build();
-            clients.get(i).write(message);
-
-            clients.get(i).expectReply();
+        /**
+         * Wait for a client to connect.
+         */
+        public void connectClient() throws Exception
+        {
+            clients.add(new Client(sock));
         }
 
-        waitClientReply();
+        public void waitClientReply() throws InterruptedException
+        {
+            // wait for all clients to have a reply
+            boolean readAll = false;
+            while (!readAll) {
+                Thread.sleep(ChatServer.TIME_POLL);
+
+                readAll = true;
+                for (Client c : clients) {
+                    readAll = readAll && c.hasReply();
+                }
+            }
+        }
+
+        /**
+         * Initialize the token ring.
+         */
+        public void init(int initialPort) throws InterruptedException
+        {
+            // give everyone the proper port number
+            for (int i = 0; i < clients.size(); i++) {
+                // create the message
+                ChatProto.Init init = ChatProto.Init.newBuilder()
+                    .setPort(initialPort + i)
+                    .build();
+                clients.get(i).write(init);
+
+                clients.get(i).expectReply();
+            }
+
+            waitClientReply();
+
+            // link all clients
+            for (int i = 0; i < clients.size(); i++) {
+                ByteString ip = ByteString.copyFrom(clients.get(i).getIp());
+                ChatProto.ConnectTo message = ChatProto.ConnectTo.newBuilder()
+                    .setIp(ip)
+                    .setPort((i + 1) % clients.size() + initialPort)
+                    .setInit(i == 0)
+                    .build();
+                clients.get(i).write(message);
+
+                clients.get(i).expectReply();
+            }
+
+            waitClientReply();
+        }
     }
-}
-
-class ChatServer {
 
     public final static int PORT = 25665;
     public final static int MAX_CLIENTS = 3;
@@ -166,9 +167,8 @@ class ChatServer {
             server.connectClient();
         }
 
+        // initialize the token ring and terminate
         server.init(PORT + 1);
-
-        System.out.println("All Acknowledges Received. Terminating Main UDP Server Now");
     }
 
     public static void main(String args[]) throws Exception, IOException, InterruptedException {
