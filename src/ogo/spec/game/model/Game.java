187530def52bd6b885683592e63d8fe77351694b
Sjef van Loo <sjefvanloo@gmail.com>
2012-12-14 11:18:26 +0100

    Game and Player basic implementation

--- src/ogo/spec/game/model/Game.java
+++ src/ogo/spec/game/model/Game.java
@@ -1,5 +1,14 @@
 package ogo.spec.game.model;
 
+import java.util.Timer;
+
 public class Game
 {
+    private Timer Timer;
+    private Player[] players;
+    
+    private void tick()
+    {
+        
+    }
 }
---
+++ src/ogo/spec/game/model/Player.java
@@ -0,0 +1,8 @@
+package ogo.spec.game.model;
+
+public class Player
+{
+    private String name;
+       
+    private Creature[] creatures;
+}
