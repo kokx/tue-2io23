5cacc40d613d39e04529d948d767d9dffd115ede
Philippe Denissen <philippedenissen@hotmail.com>
2013-01-16 13:09:14 +0100

    Added Test class to test various methods.

---
+++ src/ogo/spec/game/Test.java
@@ -0,0 +1,15 @@
+package ogo.spec.game;
+
+/**
+ * Test class to test various methods.
+ */
+public class Test {
+
+    private void run() {
+
+    }
+
+    public static void main(String args[]) {
+        new Test().run();
+    }
+}
